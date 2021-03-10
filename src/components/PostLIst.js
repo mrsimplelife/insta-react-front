71fd8312e75bb5572428491900ac193e74ed3cb3
youme <mrsimplelife@naver.com>
2021-03-11 08:47:10 +0900

    Add no results

--- src/components/PostLIst.js
+++ src/components/PostLIst.js
@@ -1,20 +1,26 @@
+import { Alert } from "antd";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useAppContext } from "store";
 import Post from "./Post";
 const apiUrl = "http://127.0.0.1:8000/api/posts/";
 function PostList() {
-  const { store } = useAppContext();
+  const {
+    store: { jwtToken },
+  } = useAppContext();
   const [postList, setPostList] = useState([]);
   useEffect(() => {
-    axios.get(apiUrl).then((res) => {
+    const headers = { Authorization: `JWT ${jwtToken}` };
+    axios.get(apiUrl, { headers }).then((res) => {
       const { data } = res;
       setPostList(data);
     });
-  }, []);
+  }, [jwtToken]);
   return (
     <>
-      {JSON.stringify(store)}
+      {postList.length === 0 && (
+        <Alert type="warning" message="포스팅이 없습니다. :-(" />
+      )}
       {postList.map((post) => (
         <Post key={post.id} post={post} />
       ))}
