7b6b7a6165ac05c8d6cda02b2d0420e7f10db5f5
youme <mrsimplelife@naver.com>
2021-03-11 09:45:40 +0900

    Add suggestion user list

--- src/components/SuggestionList.js
+++ src/components/SuggestionList.js
@@ -1,17 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Card } from "antd";
 import Suggestion from "./Suggestion";
-// import "./SuggestionList.scss";
+import { useAppContext } from "store";
+import axios from "axios";
 
 export default function SuggestionList({ style }) {
+  const {
+    store: { jwtToken },
+  } = useAppContext();
+  const [userList, setUserList] = useState([]);
+
+  useEffect(() => {
+    async function fetchUserList() {
+      const apiUrl = "http://localhost:8000/accounts/suggestions/";
+      const headers = { Authorization: `JWT ${jwtToken}` };
+      try {
+        const { data } = await axios.get(apiUrl, { headers });
+        setUserList(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchUserList();
+  }, [jwtToken]);
+
   return (
     <div style={style}>
       <Card title="Suggestions for you " size="small">
-        <Suggestion />
-        <Suggestion />
-        <Suggestion />
-        <Suggestion />
-        <Suggestion />
+        {userList.map((suggestionUser) => (
+          <Suggestion
+            key={suggestionUser.username}
+            suggestionUser={suggestionUser}
+          />
+        ))}
       </Card>
     </div>
   );
