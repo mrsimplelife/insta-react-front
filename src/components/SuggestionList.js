52f4a9e3bab97e1f82c69e5155b3eaa7b661e087
youme <mrsimplelife@naver.com>
2021-03-11 09:53:20 +0900

    Add useAxios

--- src/components/SuggestionList.js
+++ src/components/SuggestionList.js
@@ -1,39 +1,32 @@
-import React, { useEffect, useState } from "react";
-import { Card } from "antd";
+import { Button, Card } from "antd";
 import Suggestion from "./Suggestion";
 import { useAppContext } from "store";
-import axios from "axios";
+import useAxios from "axios-hooks";
 
 export default function SuggestionList({ style }) {
   const {
     store: { jwtToken },
   } = useAppContext();
-  const [userList, setUserList] = useState([]);
+  const headers = { Authorization: `JWT ${jwtToken}` };
 
-  useEffect(() => {
-    async function fetchUserList() {
-      const apiUrl = "http://localhost:8000/accounts/suggestions/";
-      const headers = { Authorization: `JWT ${jwtToken}` };
-      try {
-        const { data } = await axios.get(apiUrl, { headers });
-        setUserList(data);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-    fetchUserList();
-  }, [jwtToken]);
-
+  const [{ data: userList, loading, error }, refetch] = useAxios({
+    url: "http://localhost:8000/accounts/suggestions/",
+    headers,
+  });
   return (
     <div style={style}>
       <Card title="Suggestions for you " size="small">
-        {userList.map((suggestionUser) => (
-          <Suggestion
-            key={suggestionUser.username}
-            suggestionUser={suggestionUser}
-          />
-        ))}
+        {loading && <div>Loading ...</div>}
+        {error && <div>로딩 중에 에러가 발생했습니다.</div>}
+        {userList &&
+          userList.map((suggestionUser) => (
+            <Suggestion
+              key={suggestionUser.username}
+              suggestionUser={suggestionUser}
+            />
+          ))}
       </Card>
+      <Button onClick={refetch}>Reload</Button>
     </div>
   );
 }
