c3951508d7362df4bd532b486ce4cf3e081672ec
youme <mrsimplelife@naver.com>
2021-03-11 10:02:56 +0900

    Add user follow

--- src/components/Suggestion.js
+++ src/components/Suggestion.js
@@ -1,8 +1,8 @@
 import { Avatar, Button } from "antd";
 import "./Suggestion.scss";
 
-export default function Suggestion({ suggestionUser }) {
-  const { username, name, avatar_url } = suggestionUser;
+export default function Suggestion({ suggestionUser, onFollowUser }) {
+  const { username, name, avatar_url, is_follow } = suggestionUser;
   return (
     <div className="suggestion">
       <div className="avatar">
@@ -18,7 +18,12 @@ export default function Suggestion({ suggestionUser }) {
       </div>
       <div className="username">{name.length === 0 ? username : name}</div>
       <div className="action">
-        <Button size="small">Follow</Button>
+        {is_follow && "팔로잉 중"}
+        {!is_follow && (
+          <Button size="small" onClick={() => onFollowUser(username)}>
+            Follow
+          </Button>
+        )}
       </div>
     </div>
   );
--- src/components/SuggestionList.js
+++ src/components/SuggestionList.js
@@ -2,17 +2,34 @@ import { Button, Card } from "antd";
 import Suggestion from "./Suggestion";
 import { useAppContext } from "store";
 import useAxios from "axios-hooks";
+import { useEffect, useState } from "react";
 
 export default function SuggestionList({ style }) {
   const {
     store: { jwtToken },
   } = useAppContext();
+
   const headers = { Authorization: `JWT ${jwtToken}` };
 
-  const [{ data: userList, loading, error }, refetch] = useAxios({
+  const [userList, setUserList] = useState([]);
+
+  const [{ data: origUserList, loading, error }, refetch] = useAxios({
     url: "http://localhost:8000/accounts/suggestions/",
     headers,
   });
+  useEffect(() => {
+    if (!origUserList) return setUserList([]);
+    setUserList(origUserList.map((user) => ({ ...user, is_follow: false })));
+  }, [origUserList]);
+
+  const onFollowUser = (username) => {
+    setUserList((prevUserList) =>
+      prevUserList.map((user) =>
+        user.username !== username ? user : { ...user, is_follow: true }
+      )
+    );
+  };
+
   return (
     <div style={style}>
       <Card title="Suggestions for you " size="small">
@@ -23,6 +40,7 @@ export default function SuggestionList({ style }) {
             <Suggestion
               key={suggestionUser.username}
               suggestionUser={suggestionUser}
+              onFollowUser={onFollowUser}
             />
           ))}
       </Card>
